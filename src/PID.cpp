e3bc282ace52a07ed3ff94a6768dd7845c011c60
Marco P. Nogueira <marco.nogueira@gatech.edu>
2017-05-20 18:58:30 -0400

    PID steering implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "PID.h"
 
 using namespace std;
@@ -11,11 +12,25 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  cout << "Kp = " << Kp << " Ki = " << Ki << " Kd = " << Kd << endl;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+  cout << "p_error = " << p_error << " i_error = " << i_error << " d_error = " << d_error << endl;
 }
 
 double PID::TotalError() {
+  return p_error + d_error + i_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(0.09, 0.003, 0.9);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,9 +58,15 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+          pid.UpdateError(cte);
+          steer_value = -pid.Kp * pid.p_error - pid.Ki * pid.i_error - pid.Kd * pid.d_error;
+          if (abs(steer_value) > 1e-6) {
+            steer_value /= abs(steer_value);
+          }
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          std::cout << "Total Error: " << pid.TotalError() << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
